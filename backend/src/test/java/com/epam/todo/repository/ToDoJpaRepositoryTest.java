ad67cfc281b382849a9a3e4dbeb0dc3ccfae1535
Daniel Wolf <daniel_wolf@epam.com>
2021-05-19 05:34:10 +0200

    Use docker maven image as build tool

--- backend/src/test/java/com/epam/todo/repository/ToDoJpaRepositoryTest.java
+++ backend/src/test/java/com/epam/todo/repository/ToDoJpaRepositoryTest.java
@@ -2,14 +2,13 @@ package com.epam.todo.repository;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class ToDoJpaRepositoryTest {
 
-    @InjectMocks
-    private ToDoJpaRepository underTest;
+//    @InjectMocks
+//    private ToDoJpaRepository underTest;
 
     @Test
     void findAll() {
